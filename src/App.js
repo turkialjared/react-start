fd884e815f9c1501df2ba3ec7a72c7ce6eb707ec
turki almutairi <turkeyaljared@hotmail.com>
2023-12-20 18:36:25 +0300

    w4d1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Ui } from "./border";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <div class="App-header">
+        <Ui title=" This is Test" desc="Test I Said!!!" />
+      </div>
+      <img src="https://www.arsenalpics.com/templates/printstore/31/images/crest.jpg" />
     </div>
   );
 }
---
+++ src/border.js
@@ -0,0 +1,10 @@
+export function Ui(x) {
+  return (
+    <div>
+      <p>{x.title}</p>
+      <div>
+        <h1>{x.desc}</h1>
+      </div>
+    </div>
+  );
+}

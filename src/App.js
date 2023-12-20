2d92508ffa0c44978f12f183f780be9f7b7d46be
turki almutairi <turkeyaljared@hotmail.com>
2023-12-20 19:58:41 +0300

    done 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import "bootstrap/dist/css/bootstrap.css";
 import logo from "./logo.svg";
 import "./App.css";
 import { Ui } from "./border";
+import { JO, Tshirt } from "./products";
 
 function App() {
   return (
-    <div class="App">
-      <div class="App-header">
+    <div className="App d-flix justify-content-center">
+      <div className="App-header">
         <Ui title=" This is Test" desc="Test I Said!!!" />
       </div>
-      <img src="https://www.arsenalpics.com/templates/printstore/31/images/crest.jpg" />
+      <div>
+        <img
+          className="logo"
+          src="https://www.arsenalpics.com/templates/printstore/31/images/crest.jpg"
+        />
+      </div>
+      <div className="d-flex flex-direction-row">
+        {Tshirt.map((items) => (
+          <>
+            <div className="d-flex flex-direction-row">
+              <img src={items.link} />
+              <h1>{items.label}</h1>
+              <h3>{items.price}</h3>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,17 @@
+export const Tshirt = [
+  {
+    label: "Red T-shirt",
+    link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL7urYpKFYJ5tkxXW3MCEAD4-Y3JlwLX5ZhQ&usqp=CAU",
+    price: 25,
+  },
+  {
+    label: "White T-shirt",
+    link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpoetiecEzSvVv_T16_6FzHAD8ShoiFSfGtNUvleEMWJp1ilnFNCODjVDwBxbLhL20Qak&usqp=CAU",
+    price: 25,
+  },
+  {
+    label: "Green T-shirt",
+    link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX-w5IYYS377vTj_jdlbJaVyRnB3HNV2mk0A&usqp=CAU",
+    price: 13,
+  },
+];
